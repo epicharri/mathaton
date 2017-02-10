4663dbd966bb116326a132c907cf103f8493034c
epicharri <harri.kahkonen3@gmail.com>
2017-02-10 20:56:49 +0200

    Main -luokka luotu.

---
+++ mathaton/src/main/java/com/mathaton/mathaton/Mathaton.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mathaton.mathaton;
+
+/**
+ *
+ * @author harrikah
+ */
+public class Mathaton {
+    public static void main(String args[]){
+        System.out.println("Main -luokka luotu!");
+    }
+    
+}
